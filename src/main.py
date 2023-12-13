8307e413eb3d980c20c9869c8c104150d69b35db
Mamed Mamedov <demam777@gmail.com>
2023-12-13 22:51:13 +0300

    ok

---
+++ src/main.py
@@ -0,0 +1,49 @@
+import os
+
+from src.classes import HeadHunterAPI, SuperJobAPI, Vacancy, JSONSaver
+from src.utils import user_work_with_file
+
+
+def main():
+    # Создание экземпляра класса для работы с API сайтов с вакансиями
+    print('Привет!\n')
+    vacancies_hh = HeadHunterAPI()
+    vacancies_sj = SuperJobAPI()
+
+    # Если файл с вакансиями существует, предлагаем 2 режима работы
+    if os.path.exists('data.json'):
+        working_mode = input("1. Получить новые вакансии\n2. Работать с "
+                             "файлом\n").strip()
+    else:
+        working_mode = '1'
+    if working_mode == '1':
+
+        # Получение вакансий с разных платформ
+        vacancy = input('Введите поисковый запрос (название вакансии):\n')
+        input_platform = input(
+            'Выберите платформу, с которой будем получать вакансии:\n1. '
+            'SuperJob\n2. HeadHunter\n3. SuperJob и HeadHunter\n')
+        if input_platform == '1':
+            list_sj = vacancies_sj.get_vacancies(vacancy)
+            Vacancy.instantiate_from_data(list_sj)
+        elif input_platform == '2':
+            list_hh = vacancies_hh.get_vacancies(vacancy)
+            Vacancy.instantiate_from_data(list_hh)
+        elif input_platform == '3':
+            list_sj = vacancies_sj.get_vacancies(vacancy)
+            list_hh = vacancies_hh.get_vacancies(vacancy)
+            Vacancy.instantiate_from_data(list_sj)
+            Vacancy.instantiate_from_data(list_hh)
+        else:
+            print('\033[1;31mНужно выбрать цифру из предложенных '
+                  'вариантов!!!\033[0m')
+        json_saver = JSONSaver()
+        json_saver.save_vacancies(json_saver.json_exemplars(Vacancy.vac_list))
+        user_work_with_file()
+    # Работа с файлом
+    else:
+        user_work_with_file()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/utils.py
@@ -0,0 +1,84 @@
+from src.classes import Vacancy, JSONSaver
+
+
+def print_info():
+    """ Выводит информацию о подготовленных вакансиях """
+
+    for v in Vacancy.vac_list:
+        print(v)
+
+
+def user_work_with_file():
+    """ Функция взаимодействия с пользователем (работа с файлом) """
+    json_saver = JSONSaver()
+    while True:
+        user_choice = input(
+            'Выберите что делать?:\n0. Вывести все вакансии\n1. Вывести топ '
+            'вакансий по зарплате\n2. Отфильтровать вакансии по зарплате '
+            'начиная с самой высокооплачиваемой\n3. Отфильтровать вакансии '
+            'по городу\n4. Получить вакансии с зарплатой не ниже заданного '
+            'уровня\n5. Вывести вакансии по ключевым словам\n6. Удалить '
+            'вакансии у которых зарплата ниже заданного уровня\n7. '
+            'Закончить\n').strip()
+        if user_choice == '7':
+            print("\033[1;34mУДАЧИ В ПОИСКЕ!\033[0m")
+            exit()
+        elif user_choice == '0':
+            print_info()
+        elif user_choice == '1':
+            input_top_number = int(input('Какое количество самых '
+                                         'высокооплачиваемых'
+                                         ' вакансий показать?\n').strip())
+            json_saver.get_vacancies_salary_max()
+            for i in range(input_top_number):
+                print(Vacancy.vac_list[i])
+        elif user_choice == '2':
+            json_saver.get_vacancies_salary_max()
+            print_info()
+        elif user_choice == '3':
+            town = input('Введите город поиска:\n').strip().lower()
+            json_saver.get_vacancies_town(town)
+            print_info()
+            user_answer = input('Сохранить результат в новый файл?\n1. ДА\n2. '
+                                'НЕТ\n').strip()
+            if user_answer == '1':
+                new_file = JSONSaver('data_town.json')
+                new_file.save_vacancies(
+                    new_file.json_exemplars(Vacancy.vac_list))
+        elif user_choice == '4':
+            salary = int(input('Введите зарплату ниже которой вакансии не '
+                               'выводить:\n').strip())
+            json_saver.get_vacancies_by_salary(salary)
+            print_info()
+        elif user_choice == '5':
+            Vacancy.vac_list = []
+            filtered = list(
+                map(str, input("Введите ключевые слова для поиска в "
+                               "вакансиях (через пробел)\n").split()))
+            words = tuple(filtered)
+            Vacancy.instantiate_from_data(json_saver.get_vacancies_by_word(
+                words))
+            if not json_saver.get_vacancies_by_word(words):
+                print("\033[1;31mНет вакансий, соответствующих заданным "
+                      "критериям.\033[0m")
+            else:
+                print_info()
+                user_answer = input('Заменить результаты в файле?\n1. ДА\n2. '
+                                    'НЕТ\n').strip()
+                if user_answer == '1':
+                    json_saver.save_vacancies(
+                        json_saver.json_exemplars(Vacancy.vac_list))
+        elif user_choice == '6':
+            salary = int(input('Введите зарплату ниже которой вакансии '
+                               'удалить:\n').strip())
+            json_saver.delete_vacancy(salary)
+            print_info()
+            user_answer = input('Сохранить результат в новый файл?\n1. ДА\n2. '
+                                'НЕТ\n').strip()
+            if user_answer == '1':
+                new_file = JSONSaver('data_higher_salary.json')
+                new_file.save_vacancies(
+                    new_file.json_exemplars(Vacancy.vac_list))
+        else:
+            print('\033[1;31mНужно выбрать цифру из предложенных '
+                  'вариантов!!!\033[0m')
